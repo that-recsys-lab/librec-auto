af6e27e12020c7d8231780bc15699f3f011416f6
rburke2233 <robin.burke@colorado.edu>
2020-05-14 17:26:41 -0600

    Update conf.py
    fix bug in read the docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Fix bug in read the docs setup
+master_doc = 'index'
+
