e99ac8b890bddefe08c41e9ab21f46fbb88789b8
Mansoury <m.mansoury@tue.nl>
2020-06-30 00:41:56 +0200

    removed unnecessary files

--- build/lib/librec_auto/librec_auto/cmd/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .cmd import Cmd, SequenceCmd
-from .librec_cmd import LibrecCmd
-from .purge_cmd import PurgeCmd
-from .post_cmd import PostCmd
-from .rerank_cmd import RerankCmd
-from .status_cmd import StatusCmd
-from .parallel_cmd import ParallelCmd
-from .install_cmd import InstallCmd
--- build/lib/librec_auto/librec_auto/cmd/rerank/MMR-rerank.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import os
-import re
-import pandas as pd
-import numpy
-from librec_auto.librec_auto import read_config_file
-from pathlib import Path
-
