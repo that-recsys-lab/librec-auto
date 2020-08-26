6bbd43d5acf52e36f4d4296b1bfb048a91157456
Andrew Schwartz <andrew@aschwartz.me>
2020-08-26 14:16:43 -0600

    fix spacing in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="librec-auto",
     version="0.1.27",
-	scripts=['core\__main__.py'] ,
+    scripts=['core\__main__.py'] ,
     author="Masoud Mansoury and Robin Burke",
     author_email="masoodmansoury@gmail.com",
     description="The librec-auto project aims to automate recommender system experimens using LibRec.",
@@ -14,13 +14,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/that-recsys-lab/librec-auto",
     packages=setuptools.find_packages(),
-	include_package_data=True,
-	install_requires=[
-          'matplotlib',
-		  'pandas',
-		  'numpy',		  
+    include_package_data=True,
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'numpy',
     ],
-	classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
