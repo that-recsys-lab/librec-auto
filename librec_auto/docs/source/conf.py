a0eb0e4c0d1fc5cb08e17ef234b9159f3f421e17
Andrew Schwartz <andrew@aschwartz.me>
2020-08-31 22:51:09 -0600

    feat(docs): connect github repo, add project description

--- librec_auto/docs/source/conf.py
+++ librec_auto/docs/source/conf.py
@@ -45,6 +45,12 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    'github_user': 'that-recsys-lab',
+    'github_repo': 'librec-auto',
+    'description': 'The librec-auto project aims to automate recommender system studies using Librec.'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
