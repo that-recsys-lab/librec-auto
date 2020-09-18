c5000965313d9f6e168e8d6cbb5e41c0b614c435
rburke2233 <robin.burke@colorado.edu>
2020-09-17 22:18:59 -0600

    Updated library handling
    Now uses correct XML API. Split the system library into algorithms and metrics.

--- librec_auto/core/util/__init__.py
+++ librec_auto/core/util/__init__.py
@@ -1,5 +1,5 @@
 from .files import Files, SubPaths
-from .utils import safe_xml_path, extract_from_path, force_list, force_path, frange, confirm
+from .utils import force_list, force_path, frange, confirm
 from .xml_utils import read_xml_from_path_string, xml_load_from_path, xml_load_from_text, \
     build_parent_path, merge_elements, single_xpath
 from .log_file import LogFile
