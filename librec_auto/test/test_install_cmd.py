0402c38c5ea18add7ff95891e014c6071b2d308f
Andrew Schwartz <andrew@aschwartz.me>
2020-10-11 16:19:23 -0600

    chore(test): test more install cmd methods

--- librec_auto/test/test_install_cmd.py
+++ librec_auto/test/test_install_cmd.py
@@ -1,4 +1,3 @@
-import unittest
 import os
 from pathlib import Path
 
@@ -6,26 +5,35 @@ import librec_auto
 from librec_auto.core.cmd.install_cmd import InstallCmd
 from librec_auto.core import ConfigCmd
 
+def _get_config():
+    return ConfigCmd('config.xml', '')
 
-class TestInstallCmd(unittest.TestCase):
-    def setUp(self):
-        install_path = Path(librec_auto.__file__).parent
-        jar_path = install_path / "jar" / "auto.jar"
+def test_str():
+    install = InstallCmd()
+    assert install.__str__() == 'InstallCmd()'
 
-        # remove jar if it already exists
-        if os.path.exists(jar_path.absolute()):
-            os.remove(jar_path.absolute())
+def test_dry_run(capsys):
+    install = InstallCmd()
+    captured = capsys.readouterr()
+    print(captured.out)
+    install.dry_run(_get_config())
+    out, err = capsys.readouterr()
+    assert out == "\nlibrec-auto (DR): Running install command InstallCmd()\n"
 
-    def test_execute(self):
-        # set up the paths
-        install_path = Path(librec_auto.__file__).parent
-        jar_path = install_path / "jar" / "auto.jar"
+def test_execute():
+    install_path = Path(librec_auto.__file__).parent
+    jar_path = install_path / "jar" / "auto.jar"
 
-        # set up the config
-        config = ConfigCmd('config.xml', "")
+    # remove jar if it already exists
+    if os.path.exists(jar_path.absolute()):
+        os.remove(jar_path.absolute())
 
-        install = InstallCmd()
-        install.execute(config)
+    # set up the paths
+    install_path = Path(librec_auto.__file__).parent
+    jar_path = install_path / "jar" / "auto.jar"
 
-        # check if the jar exists
-        self.assertTrue(jar_path.absolute().exists())
+    install = InstallCmd()
+    install.execute(_get_config())
+
+    # check if the jar exists
+    assert jar_path.absolute().exists()
