cc459156e7cd453635c7dd71f229b759dc4238a1
Andrew Schwartz <andrew@aschwartz.me>
2020-10-11 16:21:58 -0600

    chore: lint

--- librec_auto/test/test_install_cmd.py
+++ librec_auto/test/test_install_cmd.py
@@ -5,12 +5,15 @@ import librec_auto
 from librec_auto.core.cmd.install_cmd import InstallCmd
 from librec_auto.core import ConfigCmd
 
+
 def _get_config():
     return ConfigCmd('config.xml', '')
+
 
 def test_str():
     install = InstallCmd()
     assert install.__str__() == 'InstallCmd()'
+
 
 def test_dry_run(capsys):
     install = InstallCmd()
@@ -19,6 +22,7 @@ def test_dry_run(capsys):
     install.dry_run(_get_config())
     out, err = capsys.readouterr()
     assert out == "\nlibrec-auto (DR): Running install command InstallCmd()\n"
+
 
 def test_execute():
     install_path = Path(librec_auto.__file__).parent
