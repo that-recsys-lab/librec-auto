641ab46f30053971abe3a0678fc814bb21710355
Andrew Schwartz <andrew@aschwartz.me>
2020-10-13 00:11:57 -0600

    fix(test): directory structure issues with test_read_config_cmd

--- librec_auto/test/core/test_config_cmd.py
+++ librec_auto/test/core/test_config_cmd.py
@@ -16,9 +16,24 @@ def test_read_config_cmd():
     # ...so the config is invalid
     assert invalid_config.is_valid() == False
 
+    # make temp directory
+    Path('librec_auto/test/temp').mkdir(exist_ok=True)
+    Path('librec_auto/test/temp/conf').mkdir(exist_ok=True)
+
+    # copy the config into the temp directory
+    current_config = Path('librec_auto/test/test-config.xml')
+    new_config = Path('librec_auto/test/temp/conf/test-config.xml')
+    shutil.copy(current_config, new_config)
+
     # this file exists
-    invalid_config = read_config_file('config.xml', 'librec_auto/test/temp')
+    invalid_config = read_config_file('test-config.xml',
+                                      'librec_auto/test/temp')
+
     # so the config is valid
     assert invalid_config.is_valid() == True
+
     # and the ._var_data attribute has been initialized
     assert invalid_config._var_data == defaultdict(list)
+
+    # break down the temp dir
+    shutil.rmtree(Path('librec_auto/test/temp'))
