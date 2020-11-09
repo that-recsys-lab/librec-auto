b17c9110fe137dca0da6e35a6629deb005c25073
Andrew Schwartz <andrew@aschwartz.me>
2020-11-09 14:02:37 -0700

    feat(test): check reranked results are correct

--- librec_auto/test/core/cmd/test_rerank_cmd.py
+++ librec_auto/test/core/cmd/test_rerank_cmd.py
@@ -1,16 +1,20 @@
+import sys
+import filecmp
 from pathlib import Path
 from shutil import copyfile, rmtree
-
-import sys
 
 from librec_auto.core.cmd.rerank_cmd import RerankCmd
 from librec_auto.core import ConfigCmd
 
 RERANK_DIR = 'librec_auto/test/core/cmd/rerank'
 
+# todo add rerank-utils as dir stem
+
 
 def test_run_script():
+    # set command line target flag
     sys.argv = ['-t', RERANK_DIR]
+
     # create file structure
 
     # mkdir rerank
@@ -34,6 +38,12 @@ def test_run_script():
     original_path = (experiment_path / Path('original'))
     original_path.mkdir(exist_ok=True)
 
+    # copy out-1.txt and out-2.txt to original directory
+    copyfile(Path('librec_auto/test/data/rerank-utils/original-out-1.txt'),
+             original_path / 'out-1.txt')
+    copyfile(Path('librec_auto/test/data/rerank-utils/original-out-2.txt'),
+             original_path / 'out-2.txt')
+
     # make experiment conf directory
     experiment_conf_path = (experiment_path / Path('conf'))
     experiment_conf_path.mkdir(exist_ok=True)
@@ -44,20 +54,18 @@ def test_run_script():
             'librec_auto/test/data/rerank-utils/rerank-experiment-config.xml'),
         experiment_conf_path / 'config.xml')
 
-    (experiment_path / Path('results')).mkdir(exist_ok=True)
+    # make results directory
+    results_path = (experiment_path / Path('result'))
+    results_path.mkdir(exist_ok=True)
 
-    # copy out-1.txt and out-2.txt to original
-    copyfile(Path('librec_auto/test/data/rerank-utils/original-out-1.txt'),
-             original_path / 'out-1.txt')
-    copyfile(Path('librec_auto/test/data/rerank-utils/original-out-2.txt'),
-             original_path / 'out-2.txt')
-
-    # create RerankCmd
+    # create config
     config = ConfigCmd(RERANK_DIR + '/conf/config.xml', RERANK_DIR)
 
+    # initialize rerankCmd
     rerank = RerankCmd()
 
     script = Path('../../../../../librec_auto/core/cmd/rerank/far_rerank.py')
+
     param_spec = ['--max_len=4', '--lambda=0.3', '--binary=False']
 
     rerank._files = config.get_files()
@@ -73,13 +81,14 @@ def test_run_script():
                                original_path=original_path,
                                param_spec=param_spec)
 
-    assert result == 0
+    assert result == 0  # check that the script ran successfully
 
-    # todo verify the reranking worked here
+    # check the reranked results are correct
+    assert filecmp.cmp(
+        Path('librec_auto/test/data/rerank-utils/result-out-1.txt'),
+        results_path / 'out-1.txt')
+    assert filecmp.cmp(
+        Path('librec_auto/test/data/rerank-utils/result-out-2.txt'),
+        results_path / 'out-2.txt')
 
     rmtree(RERANK_DIR)  # delete the reranking directory
-
-    assert 1 == 0  # display results
-
-    # import pdb
-    # pdb.set_trace()
