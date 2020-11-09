c3ec9da694a75ecadb086af960b77352e73320b8
Andrew Schwartz <andrew@aschwartz.me>
2020-11-09 14:10:54 -0700

    chore: lint

--- librec_auto/core/cmd/rerank/fairstar_rerank.py
+++ librec_auto/core/cmd/rerank/fairstar_rerank.py
@@ -3,7 +3,6 @@
 # Fall 2020
 # base on Masoud's code
 
-
 # rerank config:
 
 # < script
@@ -37,10 +36,11 @@ import fairsearchcore as fsc
 import warnings
 warnings.filterwarnings('ignore')
 
+
 class FairStarHelper():
     item_feature_df = None
     binary = True
-    
+
     alpha = 0
     prop = 0
     max_len = 0
@@ -51,9 +51,11 @@ class FairStarHelper():
 
     def is_protected(self, itemid):
         return itemid in self.protected_set
+
     def protected_count(self, items):
         num_prot = [self.is_protected(itemid) for itemid in items].count(True)
         return num_prot
+
 
 def set_helper(alpha, max_len, binary, protected, item_feature_df):
     items = item_feature_df["itemid"].to_numpy()
@@ -66,12 +68,16 @@ def set_helper(alpha, max_len, binary, protected, item_feature_df):
     helper.prop = protected_prop(helper, items)
     return helper
 
+
 def get_protected_set(item_features, helper):
-    return set((item_features[(item_features['feature'] == helper.protected)
-                              & (item_features['value'] == 1)].itemid).tolist())
+    return set(
+        (item_features[(item_features['feature'] == helper.protected)
+                       & (item_features['value'] == 1)].itemid).tolist())
+
 
 def protected_prop(helper, items):
     return helper.protected_count(items) / len(items)
+
 
 def generate_fairstar(helper):
     max_len = helper.max_len
@@ -79,6 +85,7 @@ def generate_fairstar(helper):
     alpha = helper.alpha
     fair = fsc.Fair(max_len, p, alpha)
     return fair
+
 
 def rerank(user_rating_df, fair, helper):
     all_user_id = np.unique(user_rating_df['userid'].to_numpy())
@@ -88,33 +95,40 @@ def rerank(user_rating_df, fair, helper):
     user_id = []
     reranked_id = []
     reranked_score = []
-    
+
     for user in all_user_id:
 
-        user_rating = user_rating_df.loc[user_rating_df["userid"] == user, ["rating"]].to_numpy()
+        user_rating = user_rating_df.loc[user_rating_df["userid"] == user,
+                                         ["rating"]].to_numpy()
         scaled_ratings = scaler.fit_transform(user_rating).flatten()
         unfair_list = []
-        items = user_rating_df.loc[user_rating_df["userid"] == user, ["itemid"]].to_numpy().flatten()
+        items = user_rating_df.loc[user_rating_df["userid"] == user,
+                                   ["itemid"]].to_numpy().flatten()
 
         for i in range(min(helper.max_len, len(items))):
 
             item = items[i]
             score = (scaled_ratings[i] * 100)
-            unfair_list.append(fsc.models.FairScoreDoc(item,score, False if item not in helper.protected_set else True))
-        
+            unfair_list.append(
+                fsc.models.FairScoreDoc(
+                    item, score,
+                    False if item not in helper.protected_set else True))
+
         fair.is_fair(unfair_list)
         re_ranked = fair.re_rank(unfair_list)
-        
+
         for j in range(helper.max_len):
             user_id.append(user)
             reranked_id.append(re_ranked[j].id)
             reranked_score.append(re_ranked[j].score / 100)
-    
+
     ziplist = list(zip(user_id, reranked_id, reranked_score))
     df = pd.DataFrame(ziplist, columns=['userid', 'itemid', 'rating'])
     return df
 
+
 RESULT_FILE_PATTERN = 'out-\d+.txt'
+
 
 def read_args():
     """
@@ -125,18 +139,25 @@ def read_args():
     parser.add_argument('conf', help='Name of configuration file')
     parser.add_argument('original', help='Path to original results directory')
     parser.add_argument('result', help='Path to destination results directory')
-    parser.add_argument('--max_len', help='The maximum number of items to return in each list', default=10)
+    parser.add_argument(
+        '--max_len',
+        help='The maximum number of items to return in each list',
+        default=10)
     parser.add_argument('--alpha', help='alpha.')
-    parser.add_argument('--binary', help='Whether P(\\bar{s)|d) is binary or real-valued', default=True)
+    parser.add_argument('--binary',
+                        help='Whether P(\\bar{s)|d) is binary or real-valued',
+                        default=True)
     parser.add_argument('--protected', help='protected feature', default="new")
     input_args = parser.parse_args()
     return vars(input_args)
+
 
 def enumerate_results(result_path):
     pat = re.compile(RESULT_FILE_PATTERN)
     files = [file for file in result_path.iterdir() if pat.match(file.name)]
     files.sort()
     return files
+
 
 def load_item_features(config, data_path):
     item_feature_file = single_xpath(
@@ -148,14 +169,16 @@ def load_item_features(config, data_path):
         return None
 
     item_feature_df = pd.read_csv(item_feature_path,
-                                      names=['itemid', 'feature', 'value'])
+                                  names=['itemid', 'feature', 'value'])
     #item_feature_df.set_index('itemid', inplace=True)
     return item_feature_df
+
 
 def output_reranked(reranked_df, dest_results_path, file_path):
     output_file_path = dest_results_path / file_path.name
     print('Reranking for ', output_file_path)
     reranked_df.to_csv(output_file_path, header=False, index=False)
+
 
 def main():
     args = read_args()
@@ -166,13 +189,13 @@ def main():
 
     dest_results_path = Path(args['result'])
 
-    data_dir = single_xpath(config.get_xml(), '/librec-auto/data/data-dir').text
+    data_dir = single_xpath(config.get_xml(),
+                            '/librec-auto/data/data-dir').text
 
     data_path = Path(data_dir)
     data_path = data_path.resolve()
 
     item_feature_df = load_item_features(config, data_path)
-
 
     if item_feature_df is None:
         exit(-1)
@@ -186,7 +209,8 @@ def main():
 
     for file_path in result_files:
 
-        results_df = pd.read_csv(file_path, names=['userid', 'itemid', 'rating'])
+        results_df = pd.read_csv(file_path,
+                                 names=['userid', 'itemid', 'rating'])
 
         fair = generate_fairstar(helper)
         reranked_df = rerank(results_df, fair, helper)
@@ -195,4 +219,4 @@ def main():
 
 
 if __name__ == '__main__':
-     main()
+    main()
