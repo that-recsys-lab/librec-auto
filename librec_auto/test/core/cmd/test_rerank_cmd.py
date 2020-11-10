5dfaf023eb7924a346d8c3bfb8c073cd68b5defb
Andrew Schwartz <andrew@aschwartz.me>
2020-11-10 09:40:22 -0700

    feat(test): add dry run reranker test

--- librec_auto/test/core/cmd/test_rerank_cmd.py
+++ librec_auto/test/core/cmd/test_rerank_cmd.py
@@ -1,5 +1,7 @@
 import sys
 import filecmp
+from os import name
+import re
 from pathlib import Path
 from shutil import copyfile, rmtree
 
@@ -10,8 +12,7 @@ RERANK_DIR = 'librec_auto/test/core/cmd/rerank'
 
 RERANK_DATA_DIR = 'librec_auto/test/data/rerank'
 
-
-def test_run_script():
+def build_directory_structure():
     # set command line target flag
     sys.argv = ['-t', RERANK_DIR]
 
@@ -55,6 +56,15 @@ def test_run_script():
     results_path = (experiment_path / Path('result'))
     results_path.mkdir(exist_ok=True)
 
+    return {
+        "results_path": results_path,
+        "original_path": original_path,
+    }
+
+
+def test_run_script():
+    paths = build_directory_structure()
+
     # create config
     config = ConfigCmd(RERANK_DIR + '/conf/config.xml', RERANK_DIR)
 
@@ -75,15 +85,57 @@ def test_run_script():
 
     result = rerank.run_script(script=script,
                                sub_paths=sub_paths,
-                               original_path=original_path,
+                               original_path=paths['original_path'],
                                param_spec=param_spec)
 
     assert result == 0  # check that the script ran successfully
 
     # check the reranked results are correct
     assert filecmp.cmp(RERANK_DATA_DIR / Path('result-out-1.txt'),
-                       results_path / 'out-1.txt')
+                       paths['results_path'] / 'out-1.txt')
     assert filecmp.cmp(RERANK_DATA_DIR / Path('result-out-2.txt'),
-                       results_path / 'out-2.txt')
+                       paths['results_path'] / 'out-2.txt')
 
     rmtree(RERANK_DIR)  # delete the reranking directory
+
+
+def test_dry_run(capsys):
+    captured = capsys.readouterr()  # capture stdout
+
+    build_directory_structure()
+
+    # create config
+    config = ConfigCmd(RERANK_DIR + '/conf/config.xml', RERANK_DIR)
+
+    # initialize rerankCmd
+    rerank = RerankCmd()
+
+    rerank._files = config.get_files()
+
+    rerank._files.create_exp_paths(0)  # create experiment tuple
+
+    sub_paths = rerank._files.get_exp_paths(0)
+
+    rerank._config = config  # set config
+
+    rerank.dry_run(config)
+
+    rmtree(RERANK_DIR)  # delete the reranking directory
+
+    # check stdout
+    out, err = capsys.readouterr()
+
+    # for exp0000 -> only one experiment
+    # re-rank script -> wildcard to handle absolute path, far_rerank.py
+    # parameters from config.xml
+
+    # use a wildcard for the beginning of the absolute path
+    if name == 'nt':
+        # running on Windows
+        out_pattern = r"librec-auto \(DR\): Running re-ranking command RerankCmd\(\) for exp00000\n\tRe-rank script: (.*)librec-auto\\librec_auto\\core\\cmd\\rerank\\far_rerank\.py\n\tParameters: \['--max_len=4', '--lambda=0\.3', '--binary=False']\n$"
+    else:
+        # non-Windows
+        out_pattern = r"librec-auto \(DR\): Running re-ranking command RerankCmd\(\) for exp00000\n\tRe-rank script: (.*)librec-auto\/librec_auto\/core\/cmd\/rerank\/far_rerank\.py\n\tParameters: \['--max_len=4', '--lambda=0\.3', '--binary=False']\n$"
+
+    # assert that there is a match
+    assert re.match(out_pattern, out) != None
